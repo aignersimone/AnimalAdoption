e0f643f2fb7c021bc5d882451cf6991605a1de56
Simone <simone@wimmweg.at>
2024-06-06 11:19:17 +0200

    changed file

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -5,7 +5,7 @@ export function NavBar() {
     <header className="border-b items-baseline py-4 px-5">
       <nav className="flex gap-3 text-sm">
         <Link to="/app" className="px-3 py-1 font-bold hover:bg-accent rounded-sm  tracking-tight uppercase">
-          Music App
+          Little Paws
         </Link>
 
         <Link to="/about" className="px-3 py-1 font-medium hover:bg-accent rounded-sm">
