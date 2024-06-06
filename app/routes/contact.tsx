b8f601a5901115f242d4afd7fb9605db966d40ae
laura <s2210456016@fhooe.at>
2024-06-06 13:17:14 +0200

    Content of contact.tsx

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -3,7 +3,7 @@ export default function Contact() {
     <main className="mx-auto mt-28 max-w-4xl text-justify">
       <h1>Kontaktieren Sie uns!</h1>
         <p>
-            Vielen Dank für dein Interesse an unserer Tieradoptions-Website! Wir freuen uns über deine Fragen, Anregungen und Feedback
+            Vielen Dank für dein Interesse an unserer Tieradoptions-Website! Wir freuen uns über deine Fragen, Anregungen und Feedback.
         </p>
         <div className="my-2">
             <h2>Kontaktinformation:</h2>
@@ -11,12 +11,21 @@ export default function Contact() {
             <p>4232 Hagenberg</p>
             <p>Österreich</p>
         </div>
-        scslkdncasjkdncjksandvjkanedgkadsg
-
-        <div>
-
+        <div className="my-2">
+            <p><strong>Telefon:</strong></p>
+            <p>06745 495983 485039</p>
+        </div>
+        <div className="my-2">
+            <p><strong>E-mail:</strong></p>
+            <p>petadoption@hagenberg.at</p>
         </div>
 
+        <div className="my-2">
+            <p><strong>Öffnungszeiten:</strong></p>
+            <p>Monntag bis Freitag:</p>
+            <p>9:00 - 18:00</p>
+            <p>Samstag und Sonntag geschlossen, sowie an Feiertagen</p>
+        </div>
     </main>
   );
 }
