62a905cec55639df169d8e528cf703472a4d3cc8
laura <s2210456016@fhooe.at>
2024-06-06 12:04:12 +0200

    changed navbar, and added new img from logo

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -4,8 +4,8 @@ export function NavBar() {
   return (
       <header className="border-b py-4 px-5 flex flex-col items-center justify-center">
         <div>
-          <Link to="/app" className="px-3 py-1 font-bold hover:bg-accent rounded-sm  tracking-tight uppercase">
-            <img src="app/img/little_paws.svg" alt="Logo_Littlepaws" width={100} height={100}/>
+          <Link to="/app" className="px-2 font-bold hover:bg-accent rounded-sm  tracking-tight uppercase h-3">
+            <img src="app/img/little_paws.svg" alt="Logo_Littlepaws" height={150} width={150}/>
           </Link>
         </div>
         <nav className="flex gap-3 text-sm">
