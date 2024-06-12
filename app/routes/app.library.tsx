5d22aa1082fdbda2ca2a64ee2c97666e84a78148
laura <s2210456016@fhooe.at>
2024-06-12 13:32:27 +0200

    Filterung nach Typ

--- app/routes/app.library.tsx
+++ app/routes/app.library.tsx
@@ -1,34 +1,46 @@
 import { useEffect, useState } from 'react';
-import {fetchAnimals} from "~/apis/animal-api";
-import {AnimalCard} from "~/components/animal-card";
-import {AnimalCardList} from "~/components/animal-card-list";
-
+import { fetchAnimals } from "~/apis/animal-api";
+import Tabs from "~/components/tabs";
+import { AnimalCardList } from "~/components/animal-card-list";
 
 export default function AppLibrary() {
     const [animals, setAnimals] = useState(null);
+    const [selectedTab, setSelectedTab] = useState('Dog'); // Standardmäßig den Tab 'dog' auswählen
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                // Rufe die Tierdaten ab
                 const fetchedAnimals = await fetchAnimals();
-                // Setze die geladenen Daten im State
+                // Setze die geladenen Daten im State -> Herz
                 setAnimals(fetchedAnimals);
             } catch (error) {
                 console.error('Error fetching animals:', error);
             }
         };
-
-        // Rufe die fetchData-Funktion auf, um die Daten zu laden, wenn die Komponente montiert wird
         fetchData();
     }, []);
+
+    //Tabs für die Filterung
+    const tabs = [
+        { id: 'Dog', title: 'Dogs' },
+        { id: 'Cat', title: 'Cats' },
+        { id: 'Rabbit', title: 'Rabbits' },
+    ];
+
+    //
+    const handleTabChange = (tabId) => {
+        setSelectedTab(tabId);
+    };
+
+    // Filtere wenn der Typ vom Animal übereinstimmt
+    const filteredAnimals = animals ? animals.filter(animal => animal.type == selectedTab) : [];
 
     return (
         <>
             <h1>Unsere Vergabetiere</h1>
             <p className="text-muted-foreground text-sm">Choose your partner for life.</p>
-
-            <AnimalCardList animals={animals}/>
+            <Tabs className="my-4" tabs={tabs} activeTabId={selectedTab} onValueChange={handleTabChange} />
+            <AnimalCardList animals={filteredAnimals}/>
         </>
     );
 }
