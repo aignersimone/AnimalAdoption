29e516843dd1055b0491d6e84256f2a53060fa74
laura <s2210456016@fhooe.at>
2024-06-13 18:13:39 +0200

    filterung bei Vergabetiere

--- app/routes/app.library.tsx
+++ app/routes/app.library.tsx
@@ -1,8 +1,9 @@
 import { useLoaderData } from '@remix-run/react';
 import { fetchAnimals } from "~/apis/animal-api";
 import { AnimalCardList } from "~/components/animal-card-list";
-import {LoaderFunctionArgs} from "@remix-run/node";
-
+import { LoaderFunctionArgs } from "@remix-run/node";
+import Tabs from "~/components/tabs";
+import { useState } from 'react';
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const animals = await fetchAnimals();
@@ -13,14 +14,29 @@ export default function AppLibrary() {
     const data = useLoaderData<typeof loader>();
     const animals = data.animals;
 
+    const [selectedTab, setSelectedTab] = useState('Dog'); // Standardmäßig den Tab 'Dog' auswählen
 
+    // Tabs for filtering
+    const tabs = [
+        { id: 'Dog', title: 'Dogs' },
+        { id: 'Cat', title: 'Cats' },
+        { id: 'Rabbit', title: 'Rabbits' },
+    ];
+
+    // Handle tab change
+    const handleTabChange = (tabId) => {
+        setSelectedTab(tabId);
+    };
+
+    // Filter animals based on selected tab
+    const filteredAnimals = animals ? animals.filter(animal => animal.type === selectedTab) : [];
 
     return (
         <>
             <h1>Unsere Vergabetiere</h1>
             <p className="text-muted-foreground text-sm">Choose your partner for life.</p>
-
-            <AnimalCardList animals={animals}></AnimalCardList>
+            <Tabs className="my-4" tabs={tabs} activeTabId={selectedTab} onValueChange={handleTabChange} />
+            <AnimalCardList animals={filteredAnimals} />
         </>
     );
 }
