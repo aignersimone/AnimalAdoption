a6517e36996dfcbf20a72710347e5ca9b64a1a26
Simone <simone@wimmweg.at>
2024-06-17 15:22:31 +0200

    added Comments

--- app/storage.server/db.tsx
+++
@@ -1,8 +0,0 @@
-import { createStorage } from 'unstorage';
-import fsDriver from 'unstorage/drivers/fs';
-
-export const storage = createStorage({
-  driver: fsDriver({
-    base: './.data',
-  }),
-});
